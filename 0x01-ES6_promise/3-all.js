27919931ed81ca18ead14345a941662e834475bb
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-07-25 00:21:21 +0300

    fixing indintation mistakes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	return Promise.all([
-		uploadPhoto(),
-		createUser()
-	])
-		.then((vals) => {
-			console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
-		})
-		.catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
