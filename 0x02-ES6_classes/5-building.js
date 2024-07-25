55e98da6a31a1b38733c0c76edc37314877495c3
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-07-26 02:46:56 +0300

    finished sixth task

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  /**
+   * @param {Number} sqft
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a Number');
+    }
+    this._sqft = sqft;
+  }
+}
