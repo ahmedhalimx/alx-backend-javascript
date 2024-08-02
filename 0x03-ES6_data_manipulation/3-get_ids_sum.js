a29b278740b0b42c712b4df10175af0919c6b3cb
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-08-03 00:07:38 +0300

    finished fourth task

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+export default function getStudentIdsSum(studentArray) {
+  if (studentArray instanceof Array) {
+    return studentArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+    );
+  }
+  return [];
+}
