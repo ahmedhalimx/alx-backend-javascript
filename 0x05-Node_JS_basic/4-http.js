1218e179ac6bd8e95e63af3f78152f75aa9a073f
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-11-26 04:09:15 +0200

    Trying to pass the checker

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,19 @@
 const http = require('http');
 
 const PORT = 1245;
+const HOST = 'localhost';
 
 const app = http.createServer();
 
-app.on('request', (req, res) => {
+app.on('request', (_, res) => {
   const responseMsg = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseMsg.length);
   res.statusCode = 200;
-
   res.write(Buffer.from(responseMsg));
-  res.end();
 });
 
-app.listen(PORT);
+app.listen(PORT, HOST);
 
 module.exports = app;
