ad6f69e1044a92986811f162b62f368e9073aad6
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-01-10 04:27:12 +0200

    fixed problem with child process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}\n`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}\n`);
     process.exit();
   });
   process.on('exit', () => {
