e01e8ad4982b642d31e83b81deae7f307e452889
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-01-12 23:10:55 +0200

    baloza

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-param-reassign */
 const http = require('http');
 
-const app = http.createServer();
+const PORT = 1245;
 
-app.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello ALX!');
 });
 
-app.listen(1245);
+app.listen(PORT);
 
 module.exports = app;
