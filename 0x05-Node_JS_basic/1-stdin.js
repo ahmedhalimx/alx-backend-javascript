97266062e2aad36a54bf637117295f00d97571ce
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-01-12 04:33:28 +0200

    fixed format issue

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,12 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}\n`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}\n`);
-    process.exit();
-  });
-  process.on('exit', () => {
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
-  });
-}
+  }
+});
