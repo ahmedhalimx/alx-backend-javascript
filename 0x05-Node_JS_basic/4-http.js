575d39b8ef5c56a81a42ef9ab2ae08ab3b1f5f85
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-01-12 23:04:05 +0200

    task numero 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = http.createServer();
 
 app.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello ALX!');
 });
 app.listen(1245);
